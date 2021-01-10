cb4836e3d8ea3762df8ef45c66199f6cb8ee5ac0
mehdimidox <elmehdirahimi@gmail.com>
2021-01-10 13:02:18 -0500

    chart

--- website_server/views/dashboard.php
+++ website_server/views/dashboard.php
@@ -1,12 +1,172 @@
+<!-- <div id="gauge1" class="gauge-container"></div> -->
+<!-- style="background-color: #272b30;" -->
+<div class="ibox">
+    <div class="ibox-content text-center p-md">
+
+        <h4 class="m-b-xxs">System Overview</h4>
+        <div class="d-flex justify-content-center">
+            <div class="col-sm-4">
+                <div id="cpu" class="gauge-container"></div>
+            </div>
+        </div>
+        <div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div id="disk_read" class="gauge-container"></div>
+                </div>
+                <div class="col-sm-2">
+                    <div id="disk_write" class="gauge-container"></div>
+                </div>
+                <div class="col-sm-2">
+                    <div id="ipv4_inbound" class="gauge-container"></div>
+                </div>
+                <div class="col-sm-2">
+                    <div id="ipv4_outbound" class="gauge-container"></div>
+                </div>
+                <div class="col-sm-3">
+                    <div id="used_ram" class="gauge-container"></div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<?php include('cpu.php');?>
+<?php include('disk.php');?>
 
 
-<!-- <?php include('ttt.php') ?> -->
 
 
-<?php if ($_SESSION['crm_fd_user']['fonction'] == 'admin' || $_SESSION['crm_fd_user']['fonction'] == 'manager') {
-    include('adminDatshbord.php');
-} else {
-    include('profileEmploye.php');
-}
-?>
 
+<script src="<?php echo WEB_ROOT; ?>js/plugins/guage/gauge.min.js"></script>
+<script>
+    var cpu = Gauge(
+        document.getElementById("cpu"), {
+            min: -100,
+            max: 100,
+            dialStartAngle: 180,
+            dialEndAngle: 0,
+            value: 90,
+            viewBox: "0 0 100 57",
+            color: function(value) {
+                if (value < 20) {
+                    return "#5ee432";
+                } else if (value < 40) {
+                    return "#fffa50";
+                } else if (value < 60) {
+                    return "#f7aa38";
+                } else {
+                    return "#ef4655";
+                }
+            }
+        }
+    );
+
+
+    var disk_read = Gauge(
+        document.getElementById("disk_read"), {
+            max: 100,
+            value: 60,
+            label: function(value) {
+                return Math.round(value) ;
+            },
+            color: function(value) {
+                if (value < 20) {
+                    return "#5ee432";
+                } else if (value < 40) {
+                    return "#fffa50";
+                } else if (value < 60) {
+                    return "#f7aa38";
+                } else {
+                    return "#ef4655";
+                }
+            }
+        }
+    );
+
+    var disk_write = Gauge(
+        document.getElementById("disk_write"), {
+            max: 100,
+            value: 30,
+            label: function(value) {
+                return Math.round(value) + "/" + this.max;
+            },
+            color: function(value) {
+                if (value < 20) {
+                    return "#5ee432";
+                } else if (value < 40) {
+                    return "#fffa50";
+                } else if (value < 60) {
+                    return "#f7aa38";
+                } else {
+                    return "#ef4655";
+                }
+            }
+        }
+    );
+
+    var ipv4_inbound = Gauge(
+        document.getElementById("ipv4_inbound"), {
+            max: 100,
+            value: 50,
+            label: function(value) {
+                return Math.round(value) + "/" + this.max;
+            },
+            color: function(value) {
+                if (value < 20) {
+                    return "#5ee432";
+                } else if (value < 40) {
+                    return "#fffa50";
+                } else if (value < 60) {
+                    return "#f7aa38";
+                } else {
+                    return "#ef4655";
+                }
+            }
+        }
+    );
+
+    var ipv4_outbound = Gauge(
+        document.getElementById("ipv4_outbound"), {
+            max: 100,
+            value: 20,
+            label: function(value) {
+                return Math.round(value) + "/" + this.max;
+            },
+            color: function(value) {
+                if (value < 20) {
+                    return "#5ee432";
+                } else if (value < 40) {
+                    return "#fffa50";
+                } else if (value < 60) {
+                    return "#f7aa38";
+                } else {
+                    return "#ef4655";
+                }
+            }
+        }
+    );
+
+    var used_ram = Gauge(
+        document.getElementById("used_ram"), {
+            max: 100,
+            value: 60,
+            label: function(value) {
+                return Math.round(value) + "/" + this.max;
+            },
+            color: function(value) {
+                if (value < 20) {
+                    return "#5ee432";
+                } else if (value < 40) {
+                    return "#fffa50";
+                } else if (value < 60) {
+                    return "#f7aa38";
+                } else {
+                    return "#ef4655";
+                }
+            }
+        }
+    );
+</script>
---
+++ website_server/views/disk.php
@@ -0,0 +1,58 @@
+
+    <div class="col-lg-12">
+        <div class="ibox ">
+            <div class="ibox-title">
+                <h5>Multiple Axes Line Chart Example </h5>
+                <div class="ibox-tools">
+                    <a class="collapse-link">
+                        <i class="fa fa-chevron-up"></i>
+                    </a>
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-wrench"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#" class="dropdown-item">Config option 1</a>
+                        </li>
+                        <li><a href="#" class="dropdown-item">Config option 2</a>
+                        </li>
+                    </ul>
+                    <a class="close-link">
+                        <i class="fa fa-times"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="ibox-content">
+                <div class="flot-chart">
+                    <div class="flot-chart-content" id="flot-line-chart-multi"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+  <!-- Mainly scripts -->
+  <script src="<?php echo WEB_ROOT ?>js/jquery-3.1.1.min.js"></script>
+    <script src="<?php echo WEB_ROOT ?>js/popper.min.js"></script>
+    <script src="<?php echo WEB_ROOT ?>js/bootstrap.js"></script>
+    <script src="<?php echo WEB_ROOT ?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="<?php echo WEB_ROOT ?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Flot -->
+    <script src="<?php echo WEB_ROOT ?>js/plugins/flot/jquery.flot.js"></script>
+    <script src="<?php echo WEB_ROOT ?>js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+    <script src="<?php echo WEB_ROOT ?>js/plugins/flot/jquery.flot.resize.js"></script>
+    <script src="<?php echo WEB_ROOT ?>js/plugins/flot/jquery.flot.pie.js"></script>
+    <script src="<?php echo WEB_ROOT ?>js/plugins/flot/jquery.flot.time.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="<?php echo WEB_ROOT ?>js/inspinia.js"></script>
+    <script src="<?php echo WEB_ROOT ?>js/plugins/pace/pace.min.js"></script>
+
+    <!-- Flot demo data -->
+    <script src="js/demo/flot-demo.js"></script>
+    
+
+
+
+    
