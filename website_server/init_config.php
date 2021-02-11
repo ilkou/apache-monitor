4d0d76fc10f1fe2be75763b30644c3bcd818670e
mehdimidox <elmehdirahimi@gmail.com>
2021-02-11 14:17:13 +0100

    new branche

--- website_server/init_config.php
+++ website_server/init_config.php
@@ -2,14 +2,14 @@
 require_once './library/config.php';
 require_once './library/functions.php';
 
-
-$errorMessage = '&nbsp;';
-if (isset($_POST['login']) && isset($_POST['pwd'])) {
-    $result = doLogin();
-    if ($result != '') {
-        $errorMessage = $result;
-    }
-}
+$_SESSION['crm_token'] = md5(uniqid(mt_rand(), true));
+// $errorMessage = '&nbsp;';
+// if (isset($_POST['login']) && isset($_POST['pwd'])) {
+//     $result = doLogin();
+//     if ($result != '') {
+//         $errorMessage = $result;
+//     }
+// }
 
 ?>
 
@@ -44,30 +44,40 @@ if (isset($_POST['login']) && isset($_POST['pwd'])) {
                         <p>this process is required to use the website</p>
                     </div>
                     <div class="ibox-content">
-                        <form method="post">
-                            <input type="text" value="dsdsdsd" hidden>
+                        <form method="post" action="<?php echo WEB_ROOT; ?>views/process.php?cmd=init_config">
+                            <input type="text" name="token" value="<?= $_SESSION['crm_token'] ?>" hidden>
 
-                            <div class="form-group  row"><label class="col-sm-3 col-form-label">Databese name :</label>
-                                <div class="col-sm-9"><input type="text" value="db_ensak-server_monitoring" class="form-control">
+                            <div class="form-group  row">
+                                <label class="col-sm-3 col-form-label">Database name :</label>
+                                <div class="col-sm-9">
+                                    <input type="text"  name="db_name" value="db_ensak-server_monitoring" class="form-control">
                                     <span class="form-text m-b-none"><strong>NB</strong> : the old datebase with the same name will automaticly removed</span>
                                 </div>
                             </div>
-                            <div class="form-group  row"><label class="col-sm-3 col-form-label">tables prefix :</label>
-                                <div class="col-sm-9"><input type="text" value="esm" placeholder="ensak-server_monitoring" class="form-control">
+                            <div class="form-group  row">
+                                <label class="col-sm-3 col-form-label">tables prefix :</label>
+                                <div class="col-sm-9">
+                                    <input type="text"  name="tb_prefix" value="esm" placeholder="ensak-server_monitoring" class="form-control">
                                     <span class="form-text m-b-none"><strong>NB</strong> : the old tables with the same names will automaticly removed</span>
                                 </div>
                             </div>
-                            <div class="form-group row"><label class="col-sm-3 col-form-label">Database Host :</label>
-                                <div class="col-sm-9"><input type="text" value="localhost" class="form-control" name="password">
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Database Host :</label>
+                                <div class="col-sm-9">
+                                    <input type="text"  name="db_host" value="localhost" class="form-control" name="password">
                                 </div>
                             </div>
-                            <div class="form-group  row"><label class="col-sm-3 col-form-label">Database Username :</label>
-                                <div class="col-sm-9"><input type="text"  value="root"  class="form-control">
+                            <div class="form-group  row">
+                                <label class="col-sm-3 col-form-label">Database Username :</label>
+                                <div class="col-sm-9">
+                                    <input type="text"  name="db_username" value="root" class="form-control">
                                     <span class="form-text m-b-none">The Username of the database</span>
                                 </div>
                             </div>
-                            <div class="form-group row"><label class="col-sm-3 col-form-label">Database Password :</label>
-                                <div class="col-sm-9"><input type="password" class="form-control" name="password">
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Database Password :</label>
+                                <div class="col-sm-9">
+                                    <input type="password"  name="db_passwd" class="form-control" name="password">
                                     <span class="form-text m-b-none">The Password of the database</span>
                                 </div>
                             </div>
@@ -80,31 +90,38 @@ if (isset($_POST['login']) && isset($_POST['pwd'])) {
 
 
 
-                            <div class="form-group  row"><label class="col-sm-3 col-form-label">Username :</label>
-                                <div class="col-sm-9"><input type="text" class="form-control">
+                            <div class="form-group  row">
+                                <label class="col-sm-3 col-form-label">Username :</label>
+                                <div class="col-sm-9">
+                                    <input type="text"  name="username" class="form-control">
                                     <span class="form-text m-b-none">the username </span>
                                 </div>
 
                             </div>
 
-                            <div class="form-group  row"><label class="col-sm-3 col-form-label">email :</label>
-                                <div class="col-sm-9"><input type="email" placeholder="Enter email" class="form-control"></div>
+                            <div class="form-group  row">
+                                <label class="col-sm-3 col-form-label">email :</label>
+                                <div class="col-sm-9">
+                                    <input type="email"  name="email" placeholder="Enter email" class="form-control"></div>
                             </div>
 
-                            <div class="form-group row"><label class="col-sm-3 col-form-label">Password :</label>
-                                <div class="col-sm-9"><input type="password" class="form-control" name="password"></div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Password :</label>
+                                <div class="col-sm-9">
+                                    <input type="password"  name="passwd" class="form-control" name="password"></div>
                             </div>
 
-                            <div class="form-group row"><label class="col-sm-3 col-form-label">Verify Password :</label>
-
-                                <div class="col-sm-9"><input type="password" class="form-control" name="password"></div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Verify Password :</label>
+                                <div class="col-sm-9">
+                                    <input type="password"  name="v_passwd" class="form-control" name="password"></div>
                             </div>
 
 
                             <div class="hr-line-dashed"></div>
                             <div class="form-group row">
                                 <div class="col-sm-4 col-sm-offset-2">
-                                    <button class="btn btn-white btn-sm" type="submit">Cancel</button>
+                                    <!-- <button class="btn btn-white btn-sm" type="submit">Cancel</button> -->
                                     <button class="btn btn-primary btn-sm" type="submit">Save changes</button>
                                 </div>
                             </div>
--- website_server/views/process.php
+++ website_server/views/process.php
@@ -1,5 +1,6 @@
 <?php
 
+
 require_once '../library/config.php';
 require_once '../library/functions.php';
 
@@ -23,17 +24,9 @@ switch ($cmd) {
 		updatePwd();
 		break;
 
-	case 'confirmerContrat':
-		confirmerContrat();
+	case 'init_config':
+		init_config();
 		break;
-
-	case 'accepterContrat':
-		accepterContrat();
-		break;
-	case 'updateProject':
-		updateProject();
-		break;
-
 
 	default:
 		break;
@@ -43,7 +36,7 @@ switch ($cmd) {
 
 
 
-
+/////////////////////////////////////
 
 function createUser()
 {
@@ -169,316 +162,26 @@ function updatePwd()
 	}
 	return $errorMessage;
 }
+///////
 
 
-
-
-////////////////////////////////
-
-
-
-function uploadMultiFiles($file)
+function init_config()
 {
-	$name = "";
-	header('Content-type: application/json');
 
-	$valid_exts = array('jpeg', 'jpg', 'png', 'gif', 'doc', 'txt', 'pdf'); // valid extensions
-	$max_size = 50000000 * 1024; // max file size (200kb)
-	$path = 'uploads/'; // upload directory
-	for ($a = 0; $a < count($_FILES[$file]["name"]); $a++) {
-
-		$path = 'uploads/'; // upload director
-
-		if (@is_uploaded_file($_FILES[$file]['tmp_name'][$a])) {
-			// get uploaded file extension
-			$ext = strtolower(pathinfo($_FILES[$file]['name'][$a], PATHINFO_EXTENSION));
-			// looking for format and size validity
-			if (in_array($ext, $valid_exts) and $_FILES[$file]['size'][$a] < $max_size) {
-				// unique file path
-				$path = $path . uniqid() . '.' . $ext;
-				// move uploaded file from temp to uploads directory
-
-				if (move_uploaded_file($_FILES[$file]['tmp_name'][$a], $path)) {
-
-
-					$status = 'images successfully uploaded!';
-					if ($a === 0)
-						$name = $path;
-					else
-						$name = $name . ',' . $path;
-				} else {
-					$status = 'Upload Fail: Unknown error occurred!';
-				}
-			} else {
-				$status = 'Upload Fail: Unsupported file format or It is too large to upload!';
-			}
-		} else {
-			$status = 'Upload Fail: File not uploaded!';
-		}
-
-		// echo out json encoded status
-		echo json_encode(array('status' => $status));
-	}
-	return ($name);
-}
-
-function uploadFile($file)
-{
-	header('Content-type: application/json');
-
-	$valid_exts = array('jpeg', 'jpg', 'png', 'gif', 'pdf', 'doc', 'txt', 'docx'); // valid extensions
-	$max_size = 5000000000 * 1024; // max file size (200kb)
-	$path = 'uploads/'; // upload directory
-	header('Content-type: application/json');
-
-	if (@is_uploaded_file($_FILES[$file]['tmp_name'])) {
-		// get uploaded file extension
-		$ext = strtolower(pathinfo($_FILES[$file]['name'], PATHINFO_EXTENSION));
-		// looking for format and size validity
-		if (in_array($ext, $valid_exts) and $_FILES[$file]['size'] < $max_size) {
-			// unique file path
-			$path = $path . uniqid() . '.' . $ext;
-			// move uploaded file from temp to uploads directory
-
-			if (move_uploaded_file($_FILES[$file]['tmp_name'], $path)) {
-
-
-				$status = 'images successfully uploaded!';
-				return ($path);
-			} else {
-				$status = 'Upload Fail: Unknown error occurred!';
-			}
-		} else {
-			$status = 'Upload Fail: Unsupported file format or It is too large to upload!';
-		}
-	} else {
-		$status = 'Upload Fail: File not uploaded!';
-	}
-
-	// echo out json encoded status
-	echo json_encode(array('status' => $status));
-	return ("");
-}
-
-
-
-
-
-function confirmerContrat()
-{	
 	if ($_SESSION['crm_token'] != $_POST['token'])
 		die("Invalide token");
 
+	$db_name = $_POST['db_name'];
+	$db_username = $_POST['db_username'];
+	$db_passwd = $_POST['db_passwd'];
+	$db_host = $_POST['db_host'];
+	$tb_prefix = $_POST['tb_prefix'];
+	$user_name = $_POST['username'];
+	$passwd = $_POST['passwd'];
+	$v_passwd = $_POST['v_passwd'];
+	$email = $_POST['email'];
 
-	// uploadMultiFiles("images");
-
-	$code_rdv = $_GET['code_rdv'];
-	$date_signature = "";
-	if (isset($_POST['date_signature']))
-		$date_signature = $_POST['date_signature'];
-
-	$nom_site = "";
-	if (isset($_POST['nom_site']))
-		$nom_site = $_POST['nom_site'];
-
-	$nom_domaine = "";
-	if (isset($_POST['nom_domaine']))
-		$nom_domaine = $_POST['nom_domaine'];
-
-	$second_domaine = "";
-	if (isset($_POST['']))
-		$second_domaine = $_POST['second_domaine'];
-
-	isset($_POST['creation_logo']) ?  $creation_logo = 1 : $creation_logo = 0;
-
-	$logo_existant = "";
-	if (isset($_FILES['logo_existant']))
-		$logo_existant = uploadFile('logo_existant');
-
-	$style_ecriture = "";
-	if (isset($_POST['style_ecriture']))
-		$style_ecriture = $_POST['style_ecriture'];
-
-	$couleurs_dominantes1 = "";
-	if (isset($_POST['couleurs_dominantes1']))
-		$couleurs_dominantes1 = $_POST['couleurs_dominantes1'];
-	$couleurs_dominantes2 = "";
-	if (isset($_POST['couleurs_dominantes2']))
-		$couleurs_dominantes2 = $_POST['couleurs_dominantes2'];
-	$couleurs_dominantes3 = "";
-	if (isset($_POST['couleurs_dominantes3']))
-		$couleurs_dominantes3 = $_POST['couleurs_dominantes3'];
-
-	$aspect_visuel = "";
-	if (isset($_POST['aspect_visuel']))
-		$aspect_visuel = $_POST['aspect_visuel'];
-
-
-	$rubriques = "";
-	if (isset($_POST['rubriques']))
-		$rubriques = $_POST['rubriques'];
-
-
-
-	$histoire_societe = "";
-	if (isset($_POST['histoire_societe']))
-		$histoire_societe = $_POST['histoire_societe'];
-
-	$site_aime = "";
-	if (isset($_POST['site_aime']))
-		$site_aime = $_POST['site_aime'];
-	$site_concurrents = "";
-	if (isset($_POST['site_concurrents']))
-		$site_concurrents = $_POST['site_concurrents'];
-
-
-	$logos_partenaires = "";
-	if (isset($_FILES['logos_partenaires']))
-		$logos_partenaires = uploadFile('logos_partenaires');
-
-	isset($_POST['creation_texte']) ?  $creation_texte = 1 : $creation_texte = 0;
-
-	$texte_importer = "";
-	if (isset($_FILES['texte_importer']))
-		$texte_importer = uploadFile('texte_importer');
-
-	isset($_POST['gallerie_photos']) ?  $gallerie_photos = 1 : $gallerie_photos = 0;
-
-	$photos_importer = "";
-	if (isset($_FILES['photos_importer']))
-		$photos_importer = uploadMultiFiles('photos_importer');
-
-	$option_personnalisees = "";
-	if (isset($_POST['option_personnalisees']))
-		$option_personnalisees =  $_POST['option_personnalisees'];
-
-	$creation_reseaux = "";
-	if (isset($_POST['creation_reseaux']))
-		$creation_reseaux = $_POST['creation_reseaux'];
-
-	$expressions_cles = "";
-	if (isset($_POST['expressions_cles']))
-		$expressions_cles = $_POST['expressions_cles'];
-
-	$geolocalisation = "";
-	if (isset($_POST['geolocalisation']))
-		$geolocalisation = $_POST['geolocalisation'];
-
-	$commentaire_realisation = "";
-	if (isset($_POST['commentaire_realisation']))
-		$commentaire_realisation = $_POST['commentaire_realisation'];
-
-	$carte_identite = "";
-	if (isset($_FILES['carte_identite']))
-		$carte_identite = uploadFile('carte_identite');
-
-	$contrat_recto = "";
-	if (isset($_FILES['contrat_recto']))
-		$contrat_recto = uploadFile('contrat_recto');
-
-	$contrat_verso = "";
-	if (isset($_FILES['contrat_recto']))
-		$contrat_verso = uploadFile('contrat_verso');
-
-	$cheque = "";
-	if (isset($_FILES['cheque']))
-		$cheque = uploadFile('cheque');
-
-
-
-	$num_cheque = "";
-	if (isset($_POST['num_cheque']))
-		$num_cheque = $_POST['num_cheque'];
-
-	$rib = "";
-	if (isset($_FILES['rib']))
-		$rib = uploadFile('rib');
-
-	$mandats = "";
-	if (isset($_FILES['mandats']))
-		$mandats = uploadFile('mandats');
-
-	$proces_verbal = "";
-	if (isset($_FILES['proces_verbal']))
-		$proces_verbal = uploadFile('proces_verbal');
-
-
-	$sql = "INSERT INTO contrats(code_rdv , date_signature , nom_site , nom_domaine , second_domaine ,
-	 creation_logo , logo_existant , style_ecriture , couleurs_dominantes1 ,
-	 couleurs_dominantes2 , couleurs_dominantes3 , aspect_visuel , histoire_societe ,
-	 site_aime , site_concurrents , logos_partenaires , creation_texte , texte_importer ,
-	 gallerie_photos , photos_importer , option_personnalisees , creation_reseaux , expressions_cles ,
-	 geolocalisation , commentaire_realisation , carte_identite , contrat_recto , contrat_verso ,cheque,num_cheque, rib ,
-	 mandats , proces_verbal,
-	rubriques)
-	 
-	 VALUES ('$code_rdv','$date_signature','$nom_site','$nom_domaine','$second_domaine','$creation_logo','$logo_existant',
-	 '$style_ecriture','$couleurs_dominantes1','$couleurs_dominantes2','$couleurs_dominantes3','$aspect_visuel',
-	 '$histoire_societe','$site_aime','$site_concurrents','$logos_partenaires','$creation_texte','$texte_importer',
-	 '$gallerie_photos','$photos_importer','$option_personnalisees','$creation_reseaux','$expressions_cles','$geolocalisation',
-	 '$commentaire_realisation','$carte_identite','$contrat_recto','$contrat_verso','$cheque','$num_cheque','$rib',
-	 '$mandats','$proces_verbal','$rubriques')";
-
-	dbQuery($sql);
-
-	$sql2 	=  "UPDATE rendez_vous SET status_rdv = 'confirmed_contrat' WHERE code_rdv = '$code_rdv'";
-	dbQuery($sql2);
-
-	exit(0);
+	$per_config = fopen("../library/per_config.php", "w");
+	fwrite($per_config, "<?php \n \$dbHost = 'localhost'; \n \$dbUser = '".$db_username ."'; \n \$dbPass = '".$db_passwd."'; \n \$dbName = '".$db_name."'; ?>");
+	fclose($per_config);
 }
-
-
-
-function accepterContrat()
-{
-	if ($_SESSION['crm_token'] != $_POST['token'])
-		die("Invalide token");
-	$code_rdv = $_GET['code_rdv'];
-	$login_devtech = $_POST['login_devtech'];
-	$delai =  date("Y-m-d");
-	$delai = date('Y-m-d', strtotime($delai. ' + '.$_POST['delai'].' days')); 
-
-	if (
-		$_SESSION['crm_fd_user']['fonction'] == 'manager' ||
-		$_SESSION['crm_fd_user']['fonction'] == 'admin'
-	) {
-		$sql	= "UPDATE rendez_vous SET  status_rdv= 'dev_site'  WHERE code_rdv = '$code_rdv'";
-		dbQuery($sql);
-		$sql	= "UPDATE contrats SET  login_devtech= '$login_devtech',  delai = '$delai'  WHERE code_rdv = '$code_rdv'";
-		dbQuery($sql);
-		header('Location: ../views/?v=&msg=' . urlencode('contrat est pas valideè avec succès'));
-	}
-	header('Location: ../views/?v=&msg=' . urlencode('contrat n\'est pas valideè'));
-}
-
-
-
-function updateProject()
-{
-	if ($_SESSION['crm_token'] != $_POST['token'])
-		die("Invalide token");
-	$code_rdv = $_GET['code_rdv'];
-	$status_rdv = $_GET['status_rdv'];
-
-	if ($status_rdv == 'dev_site')
-		$status_rdv = 'cr_contenue';
-	elseif ($status_rdv == 'cr_contenue')
-		$status_rdv = 'optimisation';
-
-	elseif ($status_rdv == 'optimisation')
-		$status_rdv = 'cr_reseaux';
-
-	elseif ($status_rdv == 'cr_reseaux')
-		$status_rdv = 'projet_livre';
-
-	if (
-		$_SESSION['crm_fd_user']['fonction'] == 'manager' ||
-		$_SESSION['crm_fd_user']['fonction'] == 'developpeur_tech'
-	) {
-		$sql	= "UPDATE rendez_vous SET  status_rdv= '$status_rdv'  WHERE code_rdv = '$code_rdv'";
-		dbQuery($sql);
-		header('Location: ../views/?v=&msg=' . urlencode('projet est modifiè avec  succès'));
-	}
-	header('Location: ../views/?v=&msg=' . urlencode('projet n\'est pas modifiè'));
-}
-
